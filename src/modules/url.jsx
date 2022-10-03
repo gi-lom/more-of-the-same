759a680b8c894ece0f601afa74fc86f626ab0f91
gi-lom <gl.rvg.2013@protonmail.com>
2022-10-03 15:23:34 +0200

    As before

--- src/modules/url.jsx
+++ src/modules/url.jsx
@@ -1,9 +1,10 @@
 import getRedirectURI from "./redirect";
 
 export default function urlCompare() {
-    if (window === undefined)
-        return false;
-    let ur = window.location.href;
-    let basic_ur = getRedirectURI(window.location);
-    return ur.length > basic_ur.length + 1;
+    if (window !== undefined) {
+        let ur = window.location.href;
+        let basic_ur = getRedirectURI(window.location);
+        return ur.length > basic_ur.length + 1;
+    }
+    return false
 };
