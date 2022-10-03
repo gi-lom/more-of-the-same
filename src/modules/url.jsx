bec319ecc80f560fff12aacabd4e5fb23a3fc4e4
gi-lom <gl.rvg.2013@protonmail.com>
2022-10-03 15:26:47 +0200

    As before

--- src/modules/url.jsx
+++ src/modules/url.jsx
@@ -1,7 +1,7 @@
 import getRedirectURI from "./redirect";
 
 export default function urlCompare() {
-    if (window !== undefined) {
+    if (window !== undefined && window) {
         let ur = window.location.href;
         let basic_ur = getRedirectURI(window.location);
         return ur.length > basic_ur.length + 1;
