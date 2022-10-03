f7b7724bdc974d3c160836aa5746287a53d9d626
gi-lom <gl.rvg.2013@protonmail.com>
2022-10-03 15:20:11 +0200

    Fix window undefined in url module

--- src/modules/url.jsx
+++ src/modules/url.jsx
@@ -1,6 +1,8 @@
 import getRedirectURI from "./redirect";
 
 export default function urlCompare() {
+    if (window === undefined)
+        return false;
     let ur = window.location.href;
     let basic_ur = getRedirectURI(window.location);
     return ur.length > basic_ur.length + 1;
