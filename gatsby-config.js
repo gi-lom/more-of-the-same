231f37dc3f2ccca13370c1e82527144fa93a178e
gi-lom <gl.rvg.2013@protonmail.com>
2022-10-07 17:01:31 +0200

    Add robots plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,15 @@
 module.exports = {
   siteMetadata: {
     title: `More Of The Same`,
+    description: `Select a song. Play songs that sound just like it.`,
     siteUrl: `https://moreofthesame.gatsbyjs.io`,
   },
-  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-transition-link'],
+  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-transition-link', {
+    resolve: 'gatsby-plugin-robots-txt',
+    options: {
+      host: 'https://www.example.com',
+      sitemap: 'https://www.example.com/sitemap.xml',
+      policy: [{userAgent: '*', allow: '/'}]
+    }
+  }],
 }
